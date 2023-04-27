0f165b98cc102fc2a3de08fcc0b529d32001eb3b
jimmy <jimmy@gmail.com>
2023-04-27 11:25:53 +0530

    Modify Both Ps Script File

---
+++ Git_Final_Test/Psscripts/DevTestAppPool.ps1
@@ -0,0 +1,16 @@
+﻿import-module webadministration
+
+$DEV_APP_POOL_NAME="DevGit_Final_Test"
+
+if(Test-Path IIS:\AppPools\$AppPoolName)
+{
+"AppPool is already there"
+return $true;
+}
+else
+{
+"AppPool is not present"
+"Creating new AppPool"
+New-WebAppPool "$DEV_APP_POOL_NAME=" -Force
+return $false;
+}
--- Git_Final_Test/Psscripts/DevTestScript.ps1
+++ Git_Final_Test/Psscripts/DevTestScript.ps1
@@ -1,5 +1,5 @@
 ﻿$DEV_WEBSITE_NAME=DevGit_Final_Test
-$dir = "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME=DevGit_Final_Test"
+$dir = "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME"
 if(!(Test-Path -Path $dir )){
     New-Item -ItemType directory -Path $dir
     Write-Host "New folder created"
