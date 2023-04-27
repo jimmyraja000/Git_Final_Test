bc52478498746deaa49005697a6bd26f785a37fc
jimmy <jimmy@gmail.com>
2023-04-27 11:05:02 +0530

    PsScriptAdd

---
+++ Git_Final_Test/Psscripts/DevTestScript.ps1
@@ -0,0 +1,10 @@
+﻿$DEV_WEBSITE_NAME=DevGit_Final_Test
+$dir = "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME=DevGit_Final_Test"
+if(!(Test-Path -Path $dir )){
+    New-Item -ItemType directory -Path $dir
+    Write-Host "New folder created"
+}
+else
+{
+  Write-Host "Folder already exists"
+}
