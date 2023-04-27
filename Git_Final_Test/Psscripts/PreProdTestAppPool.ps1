08a352b74d71881a386306413df88a9edceb471c
jimmy <jimmy@gmail.com>
2023-04-27 14:21:25 +0530

    added script to preprod

---
+++ Git_Final_Test/Psscripts/PreProdTestAppPool.ps1
@@ -0,0 +1,17 @@
+﻿import-module webadministration
+
+$PREPROD_APP_POOL_NAME="PreProdAPP_POOL_NAME"
+
+if(Test-Path IIS:\AppPools\$AppPoolName)
+{
+"AppPool is already there"
+return $true;
+}
+else
+{
+"AppPool is not present"
+"Creating new AppPool"
+Import-Module WebAdministration
+New-WebAppPool "$PREPROD_APP_POOL_NAME" -Force
+return $false;
+}
---
+++ Git_Final_Test/Psscripts/PreProdTestScript.ps1
@@ -0,0 +1,10 @@
+﻿$PREPROD_WEBSITE_NAME="PreProdGit_Final_Test"
+$dir = "C:\inetpub\wwwroot\$PREPROD_WEBSITE_NAME="
+if(!(Test-Path -Path $dir )){
+    New-Item -ItemType directory -Path $dir
+    Write-Host "New folder created"
+}
+else
+{
+  Write-Host "Folder already exists"
+}
