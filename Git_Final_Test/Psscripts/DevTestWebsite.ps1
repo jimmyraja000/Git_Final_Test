4589e2e396b74c34f022a544cb3750e527b531da
jimmy <jimmy@gmail.com>
2023-04-27 12:26:07 +0530

    CHANGED IN DEVTEST

--- Git_Final_Test/Psscripts/DevTestWebsite.ps1
+++ Git_Final_Test/Psscripts/DevTestWebsite.ps1
@@ -2,7 +2,7 @@
 $DEV_APP_POOL_NAME= "DevAPP_POOL_NAME"
 $DEV_WEBSITE_NAME="DevGit_Final_Test"
 
-if(Test-Path IIS:\WebSite\$DEV_WEBSITE_NAME)
+if(Test-Path IIS:\Sites\$DEV_WEBSITE_NAME)
 {
 "WebSite is already there"
 return $true;
